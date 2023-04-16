8f0c8093faee39ac5ea1d4df28488362b896a1d6
hritik_trivedi.purdue <hritik2599@gmail.com>
2023-04-16 11:36:21 -0400

    Upgraded UI, Fine Tuned App
    - Added better looking UI elements
    - fine tuned the spawning of the portal to spawn in the same height as the camera

--- Assets/Scripts/Portal/PlaceOnPlane.cs
+++ Assets/Scripts/Portal/PlaceOnPlane.cs
@@ -4,19 +4,9 @@ using UnityEngine.Events;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-/// <summary>
-/// Listens for touch events and performs an AR raycast from the screen touch point.
-/// AR raycasts will only hit detected trackables like feature points and planes.
-///
-/// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
-/// and moved to the hit position.
-/// </summary>
 [RequireComponent(typeof(ARRaycastManager))]
 public class PlaceOnPlane : MonoBehaviour
 {
-    //[SerializeField]
-    //[Tooltip("Instantiates this prefab on a plane at the touch location.")]
-    //GameObject m_PlacedPrefab;
 
     UnityEvent placementUpdate;
 
@@ -29,18 +19,6 @@ public class PlaceOnPlane : MonoBehaviour
     [SerializeField]
     GameObject ARCamera;
 
-    ///// <summary>
-    ///// The prefab to instantiate on touch.
-    ///// </summary>
-    //public GameObject placedPrefab
-    //{
-    //    get { return m_PlacedPrefab; }
-    //    set { m_PlacedPrefab = value; }
-    //}
-
-    ///// <summary>
-    ///// The object instantiated as a result of a successful raycast intersection with a plane.
-    ///// </summary>
     public GameObject spawnedObject;
 
     void Awake()
@@ -66,41 +44,13 @@ public class PlaceOnPlane : MonoBehaviour
         return false;
     }
 
-    void Update()
-    {
-        //if (!TryGetTouchPosition(out Vector2 touchPosition))
-        //    return;
-
-        //if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
-        //{
-        //    // Raycast hits are sorted by distance, so the first one
-        //    // will be the closest hit.
-        //    var hitPose = s_Hits[0].pose;
-
-        //    if (spawnedObject == null)
-        //    {
-        //        //spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
-        //        m_PlacedPrefab.SetActive(true);
-        //        spawnedObject = m_PlacedPrefab;
-        //        spawnedObject.transform.position = hitPose.position;
-        //        spawnedObject.transform.rotation = hitPose.rotation;
-        //        EventsManager.PortalSpawned();
-        //    }
-        //    else
-        //    {
-        //        //spawnedObject.transform.position = hitPose.position;
-        //        Debug.Log("Move Object");
-        //    }
-        //    placementUpdate.Invoke();
-        //}
-    }
-
     [ContextMenu("Spawn Env")]
     public void SpawnPortal()
     {
-        PortalEnvironment.transform.position = ARCamera.transform.position + ARCamera.transform.forward * 1.5f;
+        PortalEnvironment.transform.position = ARCamera.transform.position + ARCamera.transform.forward * 1.5f + new Vector3(0f, 1f,0f);
         PortalEnvironment.transform.rotation = Quaternion.Euler(new Vector3(-90f, 0f, ARCamera.transform.rotation.eulerAngles.y));
         PortalEnvironment.SetActive(true);
+        visualObject.SetActive(false);
         //spawnedObject = Instantiate(PortalEnvironment, ARCamera.transform.position, Quaternion.Euler(new Vector3(-90f, 0f, -90f)));
         //spawnedObject.SetActive(true);
         EventsManager.PortalSpawned();
--- Assets/Scripts/Utility/UIManager.cs
+++ Assets/Scripts/Utility/UIManager.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject UICanvas;
     public List<GameObject> DriverInformation;
     public List<Button> TeamButtons;
+    public GameObject generateButton;
+    public TMPro.TMP_Text Instruction;
 
     private void OnEnable()
     {
-        EventsManager.onPortalEnter += () => spawnUI(true);
-        EventsManager.onPortalExit += () => spawnUI(false);
+        EventsManager.onPortalSpawn += HideGenerateButton;
+        EventsManager.onPortalEnter += () => spawnUI(true, "Tap on team name to show details");
+        EventsManager.onPortalExit += () => spawnUI(false, "Move towards the portal");
     }
 
     private void OnDisable()
     {
-        EventsManager.onPortalEnter -= () => spawnUI(true);
-        EventsManager.onPortalExit -= () => spawnUI(false);
+        EventsManager.onPortalSpawn += HideGenerateButton;
+        EventsManager.onPortalEnter -= () => spawnUI(true, "Tap on the Team Name");
+        EventsManager.onPortalExit -= () => spawnUI(false, "Move Towards the Portal");
     }
 
     void Start() { 
@@ -37,11 +42,23 @@ public class UIManager : MonoBehaviour
         foreach (var Team in DriverInformation) { Team.SetActive(false); }
     }
 
-    private void spawnUI(bool activeState) {UICanvas.SetActive(activeState); }
+    private void spawnUI(bool activeState, string instruction) {
+        UICanvas.SetActive(activeState);
+        changeInstruction(instruction);
+    }
+
+    private void changeInstruction(string instruction)  { Instruction.text = instruction;}
 
     public void CloseButton(int teamID)
     {
         DriverInformation[teamID].SetActive(false);
         TeamButtons[teamID].gameObject.SetActive(true);
     }
+
+    private void HideGenerateButton(){ 
+        generateButton.SetActive(false);
+        changeInstruction("Move Towards the Portal");
+    }
+
+    public void QuitGame() { Application.Quit(); }
 }
